1500952cd2590e60b1a309e38f61b2d2dbbbc58a
shsobur <shsoburhossen951@gmail.com>
2025-06-06 08:03:23 +0600

    Banner design completed

--- src/Layouts/Pages/Home/Banner/Banner.jsx
+++ src/Layouts/Pages/Home/Banner/Banner.jsx
@@ -1,7 +1,29 @@
+import { Link } from "react-router";
+import SvgElement from "../../../Components/SvgElement/SvgElement";
 import "./Banner.css";
 
 const Banner = () => {
-  return <div>Banner</div>;
+  return (
+    <>
+      <section id="banner_section">
+        <div className="main_banner_section_container">
+          <SvgElement></SvgElement>
+
+          <div className="banner_title_container">
+            <h1>
+              <i>Smart Minds, Shared Goals</i>
+            </h1>
+            <p>
+              Build knowledge together in a focused and friendly environment.
+            </p>
+            <Link>
+              <button>Get Stared {"->"}</button>
+            </Link>
+          </div>
+        </div>
+      </section>
+    </>
+  );
 };
 
 export default Banner;
