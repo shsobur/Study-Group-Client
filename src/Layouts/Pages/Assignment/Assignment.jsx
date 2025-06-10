bfeb4ccdb1a611aa9ce43f32f79e206c46815a52
shsobur <shsoburhossen951@gmail.com>
2025-06-10 22:13:14 +0600

    Assignment page updated

--- src/Layouts/Pages/Assignment/Assignment.jsx
+++ src/Layouts/Pages/Assignment/Assignment.jsx
@@ -1,25 +1,29 @@
 import "./Assignment.css";
-import { useEffect, useState } from "react";
+import { useContext, useEffect, useState } from "react";
 import useAxiosPublic from "../../../Hooks/useAxiosPublic";
 import AssignCard from "../../Components/AssignCard/AssignCard";
+import { AuthContext } from "../../../Provider/AuthProvider";
 
 const Assignment = () => {
   const axiosPublic = useAxiosPublic();
+  const { user } = useContext(AuthContext);
   const [subject, setSubject] = useState("");
   const [level, setLevel] = useState("");
   const [recent, setRecent] = useState("");
   const [loading, setLoading] = useState(false);
   const [assignments, setAssignments] = useState([]);
-  console.log(loading);
 
   useEffect(() => {
     setLoading(true);
     axiosPublic
-      .get(`/get-assignment?subject=${subject}&level=${level}&recent=${recent}`)
+      .get(
+        `/get-assignment?subject=${subject}&level=${level}&recent=${recent}&userEmail=${user?.email}`
+      )
       .then((res) => {
         setAssignments(res.data);
+        setLoading(false);
       });
-  }, [axiosPublic, subject, level, recent]);
+  }, [axiosPublic, subject, level, recent, user]);
 
   return (
     <>
@@ -71,17 +75,62 @@ const Assignment = () => {
                   Filter by Recency
                 </option>
                 <option>Filter off</option>
+                <option>My Assignment</option>
                 <option>New Assignments</option>
                 <option>Old Assignments</option>
               </select>
             </div>
           </div>
 
-          <div className="assignment_card_parent_container">
-            {
-              assignments.map(assignment => <AssignCard key={assignment._id} assignment={assignment}></AssignCard>)
-            }
-          </div>
+          {/* <div>
+            <h1>No data</h1>
+          </div> */}
+
+          {loading ? (
+            <div className="loading_parent_container">
+              <div className="loading_container">
+                <p>Loading. . .</p>
+                <div className="w-12 h-12 border-4 border-dashed rounded-full animate-spin dark:border-violet-600"></div>
+              </div>
+            </div>
+          ) : (
+            <div
+              className={
+                assignments.length !== 0 && "assignment_card_parent_container"
+              }
+            >
+              {assignments.length === 0 ? (
+                <div className="no_data_message_container">
+                  <h1>
+                    No data found! <small>for -</small>
+                  </h1>
+                  <p>
+                    Subject{" "}
+                    <b>
+                      <i>{subject}</i>
+                    </b>
+                    , level{" "}
+                    <b>
+                      <i>{level}</i>
+                    </b>
+                    , and{" "}
+                    <b>
+                      <i>{recent}</i>
+                    </b>
+                  </p>
+                </div>
+              ) : (
+                <>
+                  {assignments.map((assignment) => (
+                    <AssignCard
+                      key={assignment._id}
+                      assignment={assignment}
+                    ></AssignCard>
+                  ))}
+                </>
+              )}
+            </div>
+          )}
         </div>
       </section>
     </>
